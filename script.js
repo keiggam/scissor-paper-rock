75d4e84f66bfba672d51366d83022efc96678675
keiggam <maggietriestolearn@gmail.com>
2024-07-22 16:36:28 +1000

    Functions for human choice and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+//creating an array of the choices
+
+const choices = ["scissors","paper","rock"];
+
+// create a new function named getComputerChoice
+
+function getComputerChoice() {
+    //generating a random number from 0 to the array length
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return computerChoices[randomIndex];
+
+}
+
+// Create a function that takes the user choice and returns it 
+ 
+function getHumanChoice () {
+    let userInput = prompt("Please enter rock, paper, or scissors: ").toLowerCase();
+    while (!choices.includes(userInput)) {
+        userInput = prompt("Invalid choice. Please chooose rock, paper or scissors: ").toLowerCase();
+    }
+    return userInput;
+    
+}
+
+console.log(getHumanChoice());
+
+
+
+
